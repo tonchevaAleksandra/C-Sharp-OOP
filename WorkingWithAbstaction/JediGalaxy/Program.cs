0c02d7097615c823d0dece07effc0402c319b0a8
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-22 14:42:09 +0300

    Add files to OOP- refactoring code

--- WorkingWithAbstaction/JediGalaxy/Program.cs
+++ WorkingWithAbstaction/JediGalaxy/Program.cs
@@ -1,65 +1,104 @@
 ﻿using System;
 using System.Linq;
 
-namespace P03_JediGalaxy
+namespace JediGalaxy
 {
-    class Program
+   public class Program
     {
+        private static int[,] matrix;
+        private static long sum;
         static void Main()
         {
-            int[] dimestions = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-            int x = dimestions[0];
-            int y = dimestions[1];
+            int[] dimensions = Console.ReadLine()
+                .Split(new string[] { " " },
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int r = dimensions[0];
+            int c = dimensions[1];
 
-            int[,] matrix = new int[x, y];
+            InitializeField(r, c);
 
-            int value = 0;
-            for (int i = 0; i < x; i++)
-            {
-                for (int j = 0; j < y; j++)
-                {
-                    matrix[i, j] = value++;
-                }
-            }
-
+            sum = 0;
             string command = Console.ReadLine();
-            long sum = 0;
             while (command != "Let the Force be with you")
             {
-                int[] ivoS = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-                int[] evil = Console.ReadLine().Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
-                int xE = evil[0];
-                int yE = evil[1];
-
-                while (xE >= 0 && yE >= 0)
-                {
-                    if (xE >= 0 && xE < matrix.GetLength(0) && yE >= 0 && yE < matrix.GetLength(1))
-                    {
-                        matrix[xE, yE] = 0;
-                    }
-                    xE--;
-                    yE--;
-                }
-
-                int xI = ivoS[0];
-                int yI = ivoS[1];
-
-                while (xI >= 0 && yI < matrix.GetLength(1))
-                {
-                    if (xI >= 0 && xI < matrix.GetLength(0) && yI >= 0 && yI < matrix.GetLength(1))
-                    {
-                        sum += matrix[xI, yI];
-                    }
-
-                    yI++;
-                    xI--;
-                }
+                ProcessCoordinates(command);
 
                 command = Console.ReadLine();
             }
 
             Console.WriteLine(sum);
 
+        }
+
+        private static void ProcessCoordinates(string command)
+        {
+            int[] ivosCoordinates = command.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            int[] evilCoordinates = Console.ReadLine()
+                .Split(new string[] { " " }, 
+                StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            MoveEvilPlayer(evilCoordinates);
+
+            MoveIvo(ivosCoordinates);
+        }
+
+        private static void MoveIvo(int[] ivosCoordinates)
+        {
+            int ivoRow = ivosCoordinates[0];
+            int ivoCol = ivosCoordinates[1];
+
+            while (ivoRow >= 0 && ivoCol < matrix.GetLength(1))
+            {
+                if (IsInsideTheField(ivoRow, ivoCol))
+                {
+                    sum += matrix[ivoRow, ivoCol];
+                }
+
+                ivoCol++;
+                ivoRow--;
+            }
+        }
+
+        private static void MoveEvilPlayer(int[] evilCoordinates)
+        {
+            int evilRow = evilCoordinates[0];
+            int evilCol = evilCoordinates[1];
+
+            while (evilRow >= 0 && evilCol >= 0)
+            {
+                if (IsInsideTheField(evilRow, evilCol))
+                {
+                    matrix[evilRow, evilCol] = 0;
+                }
+                evilRow--;
+                evilCol--;
+            }
+        }
+
+        private static bool IsInsideTheField(int row, int col)
+        {
+            if (row < matrix.GetLength(0) && row > 0 && col < matrix.GetLength(1) && col >= 0)
+                return true;
+            return false;
+        }
+        private static void InitializeField(int r, int c)
+        {
+            matrix = new int[r, c];
+
+            int currentNum = 0;
+            for (int row = 0; row < r; row++)
+            {
+                for (int col = 0; col < c; col++)
+                {
+                    matrix[row, col] = currentNum;
+                    currentNum++;
+                }
+            }
         }
     }
 }
