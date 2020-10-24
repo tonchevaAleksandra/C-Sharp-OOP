62b2e1b6f23cb8e9ae7c8bf85e20d9190e9cd848
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-24 14:44:54 +0300

    Add files to OOP Inheritance

---
+++ Inheritance/RandomList/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RandomList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
