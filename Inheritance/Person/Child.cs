a232c02dd4e9649261fb40bcd15389cb3f3853b9
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-10-24 17:48:42 +0300

    Add files to Inheritance

---
+++ Inheritance/Person/Child.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Person
+{
+   public class Child:Person
+    {
+        private const int CHILD_MAX_AGE = 15;
+        private int age;
+        public Child(string name, int age)
+            :base(name, age)
+        {
+           
+        }
+
+        //public override int Age
+        //{
+        //    get
+        //    {
+        //        return base.Age;
+        //    }
+        //    protected set
+        //    {
+        //        if (value <= CHILD_MAX_AGE)
+        //        {
+        //            base.Age = value;
+        //        }
+        //    }
+        //}
+    }
+}
---
+++ Inheritance/Person/Person.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Person
+{
+    public class Person
+    {
+        private const int PERSON_MIN_AGE = 0;
+        private string name;
+        private int age;
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+
+        }
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            private set
+            {
+                this.name = value;
+            }
+        }
+        public virtual int Age
+        {
+            get
+            {
+                return this.age;
+            }
+            protected set
+            {
+                if (value >= PERSON_MIN_AGE)
+                {
+                    this.age = value;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.Append(String.Format("Name: {0}, Age: {1}",
+                     this.Name,
+                     this.Age));
+
+            return sb.ToString().TrimEnd();
+
+
+        }
+
+
+    }
+}
