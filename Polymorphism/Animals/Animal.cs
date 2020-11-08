a6ba830e45607fd2c3bc711d158a3b5c16a83c6f
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-11-08 12:22:03 +0200

    Add files to Polymorphism

---
+++ Polymorphism/Animals/Animal.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Animals
+{
+    public  class Animal
+    {
+        public Animal(string name, string favouriteFood)
+        {
+            this.Name = name;
+            this.FavouriteFood = favouriteFood;
+        }
+        public string Name { get; protected set; }
+        public string FavouriteFood { get; protected set; }
+
+        public virtual string ExplainSelf()
+        {
+            return $"I am {this.Name} and my favourite food is {this.FavouriteFood}";
+        }
+    }
+}
