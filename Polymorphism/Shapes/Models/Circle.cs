da7f4b1219821ef9e6b6909fc14b7ca84df0936e
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-11-08 12:42:15 +0200

    Add files to Polymorphism

---
+++ Polymorphism/Shapes/Models/Circle.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes.Models
+{
+    public class Circle : Shape
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public override double CalculateArea()
+        {
+            return Math.PI * this.radius * this.radius;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return 2 * Math.PI * this.radius;
+        }
+
+        public sealed override string Draw()
+        {
+            return base.Draw() +"Circle";
+        }
+    }
+}
---
+++ Polymorphism/Shapes/Models/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿
+namespace Shapes.Models
+{
+    public class Rectangle : Shape
+    {
+        private double sideA;
+        private double sideB;
+        public Rectangle(double height, double width)
+        {
+            this.sideA = height;
+            this.sideB = width;
+        }
+        public override double CalculateArea()
+        {
+            return this.sideA * this.sideB;
+        }
+
+        public override double CalculatePerimeter()
+        {
+            return this.sideA * 2 + this.sideB * 2;
+        }
+
+        public sealed override string Draw()
+        {
+            return base.Draw() +"Rectangle";
+        }
+    }
+}
