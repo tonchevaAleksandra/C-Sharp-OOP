ea9bbca6057204e13ad5f950f58cdc5f2cd78071
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-11-09 23:36:11 +0200

    Add files to ExceptionHandling

---
+++ ExceptionHandling/SquareRoot/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SquareRoot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                int n = int.Parse(Console.ReadLine());
+                if (n < 0)
+                    throw new ArgumentNullException();
+                Console.WriteLine(Math.Sqrt(n));
+
+            }
+            catch (ArgumentNullException)
+            {
+                Console.WriteLine("Ivalid number");
+            }
+            finally
+            {
+                Console.WriteLine("Good bye");
+            }
+        }
+    }
+}
