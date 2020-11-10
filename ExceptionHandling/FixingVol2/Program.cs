20f89bba3099b583fe3f813c2a46280009addb9f
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-11-10 11:52:23 +0200

    Add files to ExceptionHandling

---
+++ ExceptionHandling/FixingVol2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FixingVol2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1, num2;
+            byte result;
+
+            num1 = 30;
+            num2 = 60;
+            try
+            {
+                result = Convert.ToByte(num1 * num2);
+                Console.WriteLine("{0} x {1} = {2}",num1, num2, result);
+            }
+            catch (OverflowException msg)
+            {
+                Console.WriteLine(msg.Message);
+            }
+            
+        }
+    }
+}
