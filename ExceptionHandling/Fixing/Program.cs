0a57c4fb3d18325e1f3089d51699f027844d795d
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-11-10 11:41:12 +0200

    Add file to ExceptionHandling

---
+++ ExceptionHandling/Fixing/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Fixing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] weekDays = new string[5] 
+            { 
+                "Sunday",
+                "Monday", 
+                "Tuesday", 
+                "Wednesday",
+                "Thursday" 
+            };
+            try
+            {
+                for (int i = 0; i <= 5; i++)
+                {
+                    Console.WriteLine(weekDays[i]);
+                }
+            }
+            catch (IndexOutOfRangeException ior)
+            {
+                Console.WriteLine(ior.Message);
+            }
+        }
+    }
+}
