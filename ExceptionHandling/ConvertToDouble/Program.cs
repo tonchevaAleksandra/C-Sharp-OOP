c798077e86f5fd21dedb85cd7eda0cc24c1f949a
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-11-10 12:26:08 +0200

    Add files to ExceptionHandling

---
+++ ExceptionHandling/ConvertToDouble/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConvertToDouble
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number to be converted to real number!");
+            string input = Console.ReadLine();
+            try
+            {
+                double result = Convert.ToDouble(input);
+                Console.WriteLine(result);
+
+            }
+            catch (FormatException fe)
+            {
+                Console.WriteLine(fe.Message);
+            }
+            catch(OverflowException ofe)
+            {
+                Console.WriteLine(ofe.Message);
+            }
+        }
+    }
+}
