99c6ec14bae823204337e1b47b684be09711ce35
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-11-16 19:01:09 +0200

    Add files to UnitTesting

--- UnitTesting/CarManager.Tests/CarTests.cs
+++ UnitTesting/CarManager.Tests/CarTests.cs
@@ -1,18 +1,166 @@
+using System;
+
 using NUnit.Framework;
 
 namespace Tests
 {
     public class CarTests
     {
+        private Car initialCar;
+
         [SetUp]
         public void Setup()
         {
+            this.initialCar = new Car("Ford", "Fiesta", 6.0, 50.00);
         }
 
         [Test]
-        public void Test1()
+        public void ConstructorShouldSetFuelAmount()
         {
-            Assert.Pass();
+            double expectedFuelAmount = 0;
+         
+            Assert.That(this.initialCar.FuelAmount, Is.EqualTo(expectedFuelAmount));
+          
+        }
+
+        [Test]
+        public void MakeShouldBeSetCorrectly()
+        {
+            string expectedMake = "Ford";
+            Assert.That(expectedMake, Is.EqualTo(this.initialCar.Make));
+        }
+
+        [Test]
+        public void MakeShouldThrowExcWhenValueNullOrEmpty()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new Car(null, "Fiesta", 5.00, 45.00);
+
+            }, "Make cannot be null or empty!");
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new Car(string.Empty, "Fiesta", 5.00, 45.00);
+
+            }, "Make cannot be null or empty!");
+
+        }
+
+        [Test]
+        public void ModelShouldSetCorrectly()
+        {
+            string expectedModel = "Fiesta";
+
+            Assert.That(this.initialCar.Model, Is.EqualTo(expectedModel));
+        }
+
+        [Test]
+        public void ModelShouldThrowExcWhenValueNullOrEmpty()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                Car car2 = new Car("BMW", "", 10.8, 55);
+
+            }, "Model cannot be null or empty!");
+        }
+
+        [Test]
+        public void FuelConsumptionShouldSetCorrectly()
+        {
+            double expectedFuelConsumption = 6.0;
+
+            Assert.That(this.initialCar.FuelConsumption, Is.EqualTo(expectedFuelConsumption));
+        }
+
+        [Test]
+        public void FuelConsumptionShouldThrowExcWhenValueZeroOrNegative()
+        {
+            Assert.Throws<ArgumentException>(() =>
+                {
+                    new Car("Ford", "Fiesta", 0, 50.00);
+                }, "Fuel consumption cannot be zero or negative!");
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new Car("Ford", "Fiesta", -5, 50.00);
+            }, "Fuel consumption cannot be zero or negative!");
+        }
+
+        [Test]
+       public void FuelCapacityShouldSetCorrectly()
+        {
+            double expectedCapacity = 50.00;
+
+            Assert.That(this.initialCar.FuelCapacity, Is.EqualTo(expectedCapacity));
+        }
+
+        [Test]
+        public void FuelCapacityShouldThrowExcWhenValueZeroOrNegative()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new Car("Audi", "Q7", 17.0, 0);
+            }, "Fuel capacity cannot be zero or negative!");
+
+            Assert.Throws<ArgumentException>(() =>
+            {
+                new Car("Audi", "Q7", 17.0, -5);
+            }, "Fuel capacity cannot be zero or negative!");
+        }
+
+        [Test]
+        public void RefuelShouldWorkCorrectly()
+        {
+            double expectedFuelAmount = 50;
+
+            this.initialCar.Refuel(50);
+
+            Assert.That(this.initialCar.FuelAmount, Is.EqualTo(expectedFuelAmount));
+        }
+
+        [Test]
+        public void RefuelShouldWorkEvenWithFuelAmountBiggerThenCapacity()
+        {
+            double expectedFuelAmount = 50;
+
+            this.initialCar.Refuel(60);
+
+            Assert.That(this.initialCar.FuelAmount, Is.EqualTo(expectedFuelAmount));
+        }
+
+        [TestCase(0)]
+        [TestCase(-20)]
+        public void RefuelShouldThrowExceptionWhenValueIsZeroOrNegative(double fuel)
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                this.initialCar.Refuel(fuel);
+            }, "Fuel amount cannot be zero or negative!");
+     
+        }
+
+        [Test]
+        public void DriveShouldWorkCorrectly()
+        {
+            var expectedFuel = 4.00;
+
+            this.initialCar.Refuel(10);
+            this.initialCar.Drive(100);
+
+            Assert.That(this.initialCar.FuelAmount, Is.EqualTo(expectedFuel));
+
+        }
+
+        [Test]
+        public void DriveShouldThrowExceptionWhenFuelIsNotEnoughForDistance()
+        {
+            this.initialCar.Refuel(10);
+
+            Assert.Throws<InvalidOperationException>(() =>
+            {
+                this.initialCar.Drive(300);
+            }, "You don't have enough fuel to drive!");
         }
     }
 }
--- UnitTesting/CarManager/Car.cs
+++ UnitTesting/CarManager/Car.cs
@@ -1,142 +1,141 @@
 ﻿using System;
 
-namespace CarManager
+
+public class Car
 {
-    public class Car
+    private string make;
+
+    private string model;
+
+    private double fuelConsumption;
+
+    private double fuelAmount;
+
+    private double fuelCapacity;
+
+    private Car()
     {
-        private string make;
+        this.FuelAmount = 0;
+    }
 
-        private string model;
+    public Car(string make, string model, double fuelConsumption, double fuelCapacity) : this()
+    {
+        this.Make = make;
+        this.Model = model;
+        this.FuelConsumption = fuelConsumption;
+        this.FuelCapacity = fuelCapacity;
+    }
 
-        private double fuelConsumption;
-
-        private double fuelAmount;
-
-        private double fuelCapacity;
-
-        private Car()
+    public string Make
+    {
+        get
         {
-            this.FuelAmount = 0;
+            return this.make;
         }
-
-        public Car(string make, string model, double fuelConsumption, double fuelCapacity) : this()
+        private set
         {
-            this.Make = make;
-            this.Model = model;
-            this.FuelConsumption = fuelConsumption;
-            this.FuelCapacity = fuelCapacity;
-        }
-
-        public string Make
-        {
-            get
+            if (String.IsNullOrEmpty(value))
             {
-                return this.make;
-            }
-            private set
-            {
-                if (String.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentException("Make cannot be null or empty!");
-                }
-
-                this.make = value;
-            }
-        }
-
-        public string Model
-        {
-            get
-            {
-                return this.model;
-            }
-            private set
-            {
-                if (String.IsNullOrEmpty(value))
-                {
-                    throw new ArgumentException("Model cannot be null or empty!");
-                }
-
-                this.model = value;
-            }
-        }
-
-        public double FuelConsumption
-        {
-            get
-            {
-                return this.fuelConsumption;
-            }
-            private set
-            {
-                if (value <= 0)
-                {
-                    throw new ArgumentException("Fuel consumption cannot be zero or negative!");
-                }
-
-                this.fuelConsumption = value;
-            }
-        }
-
-        public double FuelAmount
-        {
-            get
-            {
-                return this.fuelAmount;
-            }
-            private set
-            {
-                if (value < 0)
-                {
-                    throw new ArgumentException("Fuel amount cannot be negative!");
-                }
-
-                this.fuelAmount = value;
-            }
-        }
-
-        public double FuelCapacity
-        {
-            get
-            {
-                return this.fuelCapacity;
-            }
-            private set
-            {
-                if (value <= 0)
-                {
-                    throw new ArgumentException("Fuel capacity cannot be zero or negative!");
-                }
-
-                this.fuelCapacity = value;
-            }
-        }
-
-        public void Refuel(double fuelToRefuel)
-        {
-            if (fuelToRefuel <= 0)
-            {
-                throw new ArgumentException("Fuel amount cannot be zero or negative!");
+                throw new ArgumentException("Make cannot be null or empty!");
             }
 
-            this.FuelAmount += fuelToRefuel;
-
-            if (this.FuelAmount > this.FuelCapacity)
-            {
-                this.FuelAmount = this.FuelCapacity;
-            }
-        }
-
-        public void Drive(double distance)
-        {
-            double fuelNeeded = (distance / 100) * this.FuelConsumption;
-
-            if (fuelNeeded > this.FuelAmount)
-            {
-                throw new InvalidOperationException("You don't have enough fuel to drive!");
-            }
-
-            this.FuelAmount -= fuelNeeded;
+            this.make = value;
         }
     }
+
+    public string Model
+    {
+        get
+        {
+            return this.model;
+        }
+        private set
+        {
+            if (String.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException("Model cannot be null or empty!");
+            }
+
+            this.model = value;
+        }
+    }
+
+    public double FuelConsumption
+    {
+        get
+        {
+            return this.fuelConsumption;
+        }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Fuel consumption cannot be zero or negative!");
+            }
+
+            this.fuelConsumption = value;
+        }
+    }
+
+    public double FuelAmount
+    {
+        get
+        {
+            return this.fuelAmount;
+        }
+        private set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Fuel amount cannot be negative!");
+            }
+
+            this.fuelAmount = value;
+        }
+    }
+
+    public double FuelCapacity
+    {
+        get
+        {
+            return this.fuelCapacity;
+        }
+        private set
+        {
+            if (value <= 0)
+            {
+                throw new ArgumentException("Fuel capacity cannot be zero or negative!");
+            }
+
+            this.fuelCapacity = value;
+        }
+    }
+
+    public void Refuel(double fuelToRefuel)
+    {
+        if (fuelToRefuel <= 0)
+        {
+            throw new ArgumentException("Fuel amount cannot be zero or negative!");
+        }
+
+        this.FuelAmount += fuelToRefuel;
+
+        if (this.FuelAmount > this.FuelCapacity)
+        {
+            this.FuelAmount = this.FuelCapacity;
+        }
+    }
+
+    public void Drive(double distance)
+    {
+        double fuelNeeded = (distance / 100) * this.FuelConsumption;
+
+        if (fuelNeeded > this.FuelAmount)
+        {
+            throw new InvalidOperationException("You don't have enough fuel to drive!");
+        }
+
+        this.FuelAmount -= fuelNeeded;
+    }
 }
+
