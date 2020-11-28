72a252c0633e675c3c475ce0733d1290cc1822b4
Aleksandra Toncheva <aleksandra_toncheva@yahoo.com>
2020-11-28 16:35:41 +0200

    Add files to Exams

--- OOPBasicRetakeExam19April2019/PlayersAndMonsters/Core/Engine.cs
+++ OOPBasicRetakeExam19April2019/PlayersAndMonsters/Core/Engine.cs
@@ -24,41 +24,43 @@ namespace PlayersAndMonsters.Core
             {
                 try
                 {
-                    string[] arguments = this.reader.ReadLine().Split(" ",StringSplitOptions.RemoveEmptyEntries);
+                    string[] arguments = this.reader.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
                     string command = arguments[0];
-                 
+
+                    if (command == "Exit")
+                    {
+                        Environment.Exit(0);
+                    }
                     if (command == "AddPlayer")
                     {
                         this.writer.WriteLine(this.controller.AddPlayer(arguments[1], arguments[2]));
                     }
-                   
-                    else if (command=="AddCard")
+
+                    else if (command == "AddCard")
                     {
                         this.writer.WriteLine(this.controller.AddCard(arguments[1], arguments[2]));
                     }
-                    //•	AddPlayerCard {username} {card name}
-                    else if (command== "AddPlayerCard")
+                   
+                    else if (command == "AddPlayerCard")
                     {
                         this.writer.WriteLine(this.controller.AddPlayerCard(arguments[1], arguments[2]));
                     }
-                    //•	Fight {attack user} {enemy user}
-                    else if (command== "Fight")
+                   
+                    else if (command == "Fight")
                     {
                         this.writer.WriteLine(this.controller.Fight(arguments[1], arguments[2]));
                     }
-                    else if(command=="Report")
+
+                    else if (command == "Report")
                     {
                         this.writer.WriteLine(this.controller.Report());
-                      
+
                     }
-                    else if(command=="Exit")
-                    {
-                        break;
-                    }
+
                 }
-                catch (Exception ex)
+                catch (ArgumentException ae)
                 {
-                    this.writer.WriteLine(ex.Message);
+                    this.writer.WriteLine(ae.Message);
                 }
 
             }
